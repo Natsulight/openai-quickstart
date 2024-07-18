7e64de2ee8a2baef48cf262ad26001bfbfd3b994
Cedric Chen <chence7@oocl.com>
2024-07-18 15:45:42 +0800

    complete the homework of "OpenAI-Translator"v2.0 - add "source_language"  argument

--- openai-translator/ai_translator/main.py
+++ openai-translator/ai_translator/main.py
@@ -18,11 +18,11 @@ if __name__ == "__main__":
     api_key = args.openai_api_key if args.openai_api_key else config['OpenAIModel']['api_key']
     model = OpenAIModel(model=model_name, api_key=api_key)
 
-
     pdf_file_path = args.book if args.book else config['common']['book']
     file_format = args.file_format if args.file_format else config['common']['file_format']
+    source_language = args.source_language if args.source_language else config['common']['source_language']
     target_language = args.target_language if args.target_language else config['common']['target_language']
 
     # 实例化 PDFTranslator 类，并调用 translate_pdf() 方法
     translator = PDFTranslator(model)
-    translator.translate_pdf(pdf_file_path, file_format, target_language)
+    translator.translate_pdf(pdf_file_path, file_format, source_language, target_language)
